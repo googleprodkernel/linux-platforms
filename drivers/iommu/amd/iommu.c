124bf05214aec693d2a815bd86c94a8379fea472
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2024-01-28 21:53:16 -0600

    iommu/amd: Introduce helper function for modifying extended IRTE
    The extended interrupt remapping table entry uses the same format as
    the normal interrupt remapping table entry in GA mode.
    
    Therefore, revamp the current modify_irte_ga() to also support
    extended IRTE.
    
    Signed-off-by: Suravee Suthiklupanit <suravee.suthikulpanit@amd.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    (cherry picked from commit 83337a1d68858adcc6a6de31ab2beb5b393aac9a)

--- drivers/iommu/amd/iommu.c
+++ drivers/iommu/amd/iommu.c
@@ -3516,16 +3516,48 @@ static int __modify_irte_ga(struct amd_iommu *iommu, u16 devid, int index,
 	return 0;
 }
 
+static int __modify_ext_irte_ga(struct amd_iommu *iommu, u32 ext_id,
+				struct irte_ga *irte)
+{
+	u128 old;
+	int ret;
+	struct ext_irte *eirte = amd_viommu_get_ext_irte(iommu, ext_id);
+
+	if (!eirte || !eirte->entry_ptr) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	/*
+	 * We use cmpxchg16 to atomically update the 128-bit IRTE,
+	 * and it cannot be updated by the hardware or other processors
+	 * behind us, so the return value of cmpxchg16 should be the
+	 * same as the old value.
+	 */
+	old = eirte->entry_ptr->irte;
+	WARN_ON(!try_cmpxchg128(&eirte->entry_ptr->irte, &old, irte->irte));
+	ret = 0;
+out:
+	return ret;
+}
+
 static int modify_irte_ga(struct amd_iommu *iommu, u16 devid, int index,
-			  struct irte_ga *irte)
+			  struct irte_ga *irte,
+			  struct amd_ir_data *ir_data)
 {
 	bool ret;
 
-	ret = __modify_irte_ga(iommu, devid, index, irte);
-	if (ret)
-		return ret;
-
-	iommu_flush_irt_and_complete(iommu, devid);
+	if (ir_data && ir_data->is_ext) {
+		ret = __modify_ext_irte_ga(iommu, ir_data->ext_id, irte);
+		if (ret)
+			return ret;
+		iommu_flush_irt_and_complete(iommu, iommu->devid);
+	} else {
+		ret = __modify_irte_ga(iommu, devid, index, irte);
+		if (ret)
+			return ret;
+		iommu_flush_irt_and_complete(iommu, devid);
+	}
 
 	return 0;
 }
@@ -3608,7 +3640,7 @@ static void irte_ga_activate(struct amd_iommu *iommu, void *entry, u16 devid, u1
 	struct irte_ga *irte = (struct irte_ga *) entry;
 
 	irte->lo.fields_remap.valid = 1;
-	modify_irte_ga(iommu, devid, index, irte);
+	modify_irte_ga(iommu, devid, index, irte, NULL);
 }
 
 static void irte_deactivate(struct amd_iommu *iommu, void *entry, u16 devid, u16 index)
@@ -3624,7 +3656,7 @@ static void irte_ga_deactivate(struct amd_iommu *iommu, void *entry, u16 devid, 
 	struct irte_ga *irte = (struct irte_ga *) entry;
 
 	irte->lo.fields_remap.valid = 0;
-	modify_irte_ga(iommu, devid, index, irte);
+	modify_irte_ga(iommu, devid, index, irte, NULL);
 }
 
 static void irte_set_affinity(struct amd_iommu *iommu, void *entry, u16 devid, u16 index,
@@ -3648,7 +3680,7 @@ static void irte_ga_set_affinity(struct amd_iommu *iommu, void *entry, u16 devid
 					APICID_TO_IRTE_DEST_LO(dest_apicid);
 		irte->hi.fields.destination =
 					APICID_TO_IRTE_DEST_HI(dest_apicid);
-		modify_irte_ga(iommu, devid, index, irte);
+		modify_irte_ga(iommu, devid, index, irte, NULL);
 	}
 }
 
@@ -3998,7 +4030,7 @@ int amd_iommu_activate_guest_mode(void *data)
 	entry->lo.fields_vapic.ga_tag      = ir_data->ga_tag;
 
 	return modify_irte_ga(ir_data->iommu, ir_data->irq_2_irte.devid,
-			      ir_data->irq_2_irte.index, entry);
+			      ir_data->irq_2_irte.index, entry, ir_data);
 }
 EXPORT_SYMBOL(amd_iommu_activate_guest_mode);
 
@@ -4033,7 +4065,7 @@ int amd_iommu_deactivate_guest_mode(void *data)
 				APICID_TO_IRTE_DEST_HI(cfg->dest_apicid);
 
 	return modify_irte_ga(ir_data->iommu, ir_data->irq_2_irte.devid,
-			      ir_data->irq_2_irte.index, entry);
+			      ir_data->irq_2_irte.index, entry, ir_data);
 }
 EXPORT_SYMBOL(amd_iommu_deactivate_guest_mode);
 
@@ -4218,8 +4250,11 @@ int amd_iommu_update_ga(int cpu, bool is_run, void *data)
 	}
 	entry->lo.fields_vapic.is_run = is_run;
 
-	return __modify_irte_ga(ir_data->iommu, ir_data->irq_2_irte.devid,
-				ir_data->irq_2_irte.index, entry);
+	if (!ir_data->is_ext)
+		return __modify_irte_ga(ir_data->iommu, ir_data->irq_2_irte.devid,
+					ir_data->irq_2_irte.index, entry);
+	else
+		return __modify_ext_irte_ga(ir_data->iommu, ir_data->ext_id, entry);
 }
 EXPORT_SYMBOL(amd_iommu_update_ga);
 #endif
