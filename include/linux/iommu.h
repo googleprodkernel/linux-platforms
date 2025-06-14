55251046dc08ae3234fe7b0d520595c8476dcfce
Nicolin Chen <nicolinc@nvidia.com>
2025-06-13 23:35:18 -0700

    iommu: Introduce get_viommu_size and viommu_init ops
    So far, a vIOMMU object has been allocated by IOMMU driver and initialized
    with the driver-level structure, before it returns to the iommufd core for
    core-level structure initialization. It has been requiring iommufd core to
    expose some core structure/helpers in its driver.c file, which result in a
    size increase of this driver module.
    
    Meanwhile, IOMMU drivers are now requiring more vIOMMU-base structures for
    some advanced feature, such as the existing vDEVICE and a future HW_QUEUE.
    Initializing a core-structure later than driver-structure gives for-driver
    helpers some trouble, when they are used by IOMMU driver assuming that the
    new structure (including core) are fully initialized, for example:
    
    core:	viommu = ops->viommu_alloc();
    driver:	// my_viommu is successfully allocated
    driver:	my_viommu = iommufd_viommu_alloc(...);
    driver:	// This may crash if it reads viommu->ictx
    driver:	new = iommufd_new_viommu_helper(my_viommu->core ...);
    core:	viommu->ictx = ucmd->ictx;
    core:	...
    
    To ease such a condition, allow the IOMMU driver to report the size of its
    vIOMMU structure, let the core allocate a vIOMMU object and initialize the
    core-level structure first, and then hand it over the driver to initialize
    its driver-level structure.
    
    Thus, this requires two new iommu ops, get_viommu_size and viommu_init, so
    iommufd core can communicate with drivers to replace the viommu_alloc op:
    
    core:	viommu = ops->get_viommu_size();
    driver:	return VIOMMU_STRUCT_SIZE();
    core:	viommu->ictx = ucmd->ictx; // and others
    core:	rc = ops->viommu_init();
    driver:	// This is safe now as viommu->ictx is inited
    driver:	new = iommufd_new_viommu_helper(my_viommu->core ...);
    core:	...
    
    This also adds a VIOMMU_STRUCT_SIZE macro, for drivers to use, which would
    statically sanitize the driver structure.
    
    Link: https://patch.msgid.link/r/3ab52c5b622dad476c43b1b1f1636c8b902f1692.1749882255.git.nicolinc@nvidia.com
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Pranjal Shrivastava <praan@google.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
     Conflicts:
    	include/linux/iommu.h
    
    (cherry picked from commit ce858cc5f7d660e7880e32624ebcb170bd8a113d)

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -14,6 +14,7 @@
 #include <linux/err.h>
 #include <linux/of.h>
 #include <linux/iova_bitmap.h>
+#include <uapi/linux/iommufd.h>
 
 #define IOMMU_READ	(1 << 0)
 #define IOMMU_WRITE	(1 << 1)
@@ -611,6 +612,16 @@ iommu_copy_struct_from_full_user_array(void *kdst, size_t kdst_entry_size,
  * @remove_dev_pasid: Remove any translation configurations of a specific
  *                    pasid, so that any DMA transactions with this pasid
  *                    will be blocked by the hardware.
+ * @get_viommu_size: Get the size of a driver-level vIOMMU structure for a given
+ *                   @dev corresponding to @viommu_type. Driver should return 0
+ *                   if vIOMMU isn't supported accordingly. It is required for
+ *                   driver to use the VIOMMU_STRUCT_SIZE macro to sanitize the
+ *                   driver-level vIOMMU structure related to the core one
+ * @viommu_init: Init the driver-level struct of an iommufd_viommu on a physical
+ *               IOMMU instance @viommu->iommu_dev, as the set of virtualization
+ *               resources shared/passed to user space IOMMU instance. Associate
+ *               it with a nesting @parent_domain. It is required for driver to
+ *               set @viommu->ops pointing to its own viommu_ops
  * @viommu_alloc: Allocate an iommufd_viommu on a physical IOMMU instance behind
  *                the @dev, as the set of virtualization resources shared/passed
  *                to user space IOMMU instance. And associate it with a nesting
@@ -668,6 +679,10 @@ struct iommu_ops {
 	void (*remove_dev_pasid)(struct device *dev, ioasid_t pasid,
 				 struct iommu_domain *domain);
 
+	size_t (*get_viommu_size)(struct device *dev,
+				  enum iommu_viommu_type viommu_type);
+	int (*viommu_init)(struct iommufd_viommu *viommu,
+			   struct iommu_domain *parent_domain);
 	struct iommufd_viommu *(*viommu_alloc)(
 		struct device *dev, struct iommu_domain *parent_domain,
 		struct iommufd_ctx *ictx, unsigned int viommu_type);
